b3940a97902972d7d42c8ce06b09204327378337
Peterson <peterson.tubini@acklenavenue.com>
2023-02-27 09:52:51 -0300

    Add project files.

---
+++ TestProject.Domain/Location.cs
@@ -0,0 +1,11 @@
+﻿using TestProject.Domain.Common;
+
+namespace TestProject.Domain
+{
+    public class Location : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime OpenTime { get; set; }
+        public DateTime CloseTime { get; set; }
+    }
+}
